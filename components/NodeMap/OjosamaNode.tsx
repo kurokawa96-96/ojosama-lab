8b3e2bfdd8092c32fae29602437288f3044cbd20
kurokawa96-96 <22wwnk65ny@privaterelay.appleid.com>
2026-09-09 10:57:23 +0900

    Update OjosamaNode.tsx

--- components/NodeMap/OjosamaNode.tsx
+++ components/NodeMap/OjosamaNode.tsx
@@ -12,7 +12,7 @@ export default function OjosamaNode({
       style={{
         padding: data.isCenter ? "24px 32px" : "16px 24px",
         borderRadius: "50%",
-        border: `３px solid ${
+        border: `3px solid ${
           data.selected ? "var(--color-accent)" : "var(--color-border)"
         }`,
         background: "var(--color-bg)",
